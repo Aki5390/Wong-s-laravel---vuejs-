23382615d08f0fdbc29f5072f1393b7732fa3684
Aki5390 <toman9632@gmail.com>
2024-12-03 14:31:59 +0800

    28. default layouts
    some change with new vue3

--- larazillow11 - Copy - Copy/resources/js/Pages/Index/Show.vue
+++ larazillow11 - Copy - Copy/resources/js/Pages/Index/Show.vue
@@ -9,9 +9,4 @@
   import { Link } from '@inertiajs/vue3'
   </script>
 
-  <script>
-  import MainLayout from '../../Layouts/MainLayout.vue';
-  export default {
-    layout: MainLayout
-  }
-  </script>
+ 
